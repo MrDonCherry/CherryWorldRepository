710a1b5f07fcfd561287af373b1febdec132eb64
MamaPapa <MamaPapa@DESKTOP-2FA8B23.fritz.box>
2020-11-02 15:01:19 +0100

    example project

---
+++ EDrive/EDrive/ScenarioScript.m
@@ -0,0 +1,19 @@
+#Object data
+
+VehicleStartWeight = 1500;
+DriverWeight = 75;
+MaxVelocity = 4;
+MaxDeceleration = 9;
+RequiredDistance = 150000;
+WeightToMove = 250;
+
+#results File
+time [2]= (1,2);
+
+if (X == 1)
+  do_something ();
+elseif (X == 2)
+  do_something_else ();
+else
+  do_something_completely_different ();
+endif
